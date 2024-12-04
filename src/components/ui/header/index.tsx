579f4f6aa91b451252f71cc64397d3a74b761bc9
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-12-04 12:04:27 -0300

    add header

--- src/components/ui/header/index.tsx
+++ src/components/ui/header/index.tsx
@@ -1,8 +1,34 @@
+"use client";
+
+import styled from "styled-components";
+import Container from "@/components/layout/container";
+import Logo from "@/components/shared/logo";
+import Link from "next/link";
+
+const Root = styled.header`
+  width: 100%;
+  height: auto;
+  background-color: ${(props) => props.theme.colors.default.primary};
+`;
+const Content = styled.header`
+  width: 100%;
+  height: 6rem;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
 const Header = () => {
   return (
-    <header>
-      <div>Header</div>
-    </header>
+    <Root>
+      <Container>
+        <Content>
+          <Link href="/">
+            <Logo width="96" height="42" />
+          </Link>
+        </Content>
+      </Container>
+    </Root>
   );
 };
 
