38ef8174aab5b4f341e23cd82a35c79f09cc5fc5
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-12-10 23:38:09 -0300

    add component input

---
+++ src/components/input/index.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+type InputProps = {
+  name: string;
+  placeholder?: string;
+  type: "email" | "password" | "text";
+};
+
+const Root = styled.input<InputProps>`
+  width: 100%;
+  height: 100%;
+  background-color: ${({ theme }) => theme.colors.default.primary};
+  border: 1px solid ${({ theme }) => theme.colors.default.border};
+  padding: 0 1.2rem;
+  border-radius: inherit;
+  font-weight: 300;
+  outline: none;
+  color: ${({ theme }) => theme.colors.default.text};
+
+  &::placeholder {
+    color: ${({ theme }) => theme.colors.default.textSecondary};
+  }
+`;
+
+const Input = ({ placeholder, name, type }: InputProps) => {
+  return <Root type={type} name={name} placeholder={placeholder} />;
+};
+
+export default Input;
