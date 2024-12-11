171969ecaf7553961c9dccee2fc21b0097879f03
Elizeu Lopes <scarabellot.lopes@gmail.com>
2024-12-10 23:37:37 -0300

    add component button with icon

---
+++ src/components/buttonWithIcon/index.tsx
@@ -0,0 +1,53 @@
+import { ReactNode } from "react";
+import styled from "styled-components";
+
+type ButtomWithIcomProps = {
+  text?: string;
+  icon?: ReactNode;
+  type?: "button" | "submit" | "reset";
+};
+
+const Root = styled.button<ButtomWithIcomProps>`
+  border: 0;
+  font-weight: 400;
+  cursor: pointer;
+
+  div {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  ${({ text, icon, theme }) =>
+    text && icon
+      ? `
+      width: 8.2rem;
+      height: 3.4rem;  
+      background-color: ${theme.colors.default.text}; 
+      color:${theme.colors.default.primary}; 
+      border-radius: 1.6rem;
+      display:flex;
+      justify-content:center;
+      align-items:center;
+      gap:.4rem;
+      font-weight: 600;
+      font-size: 1.4rem;
+      transition: all .3s ease;
+
+      &:hover{
+        background-color: ${theme.colors.default.backgroundYellow}; 
+      }
+      `
+      : `background-color: transparent;`}
+`;
+
+const ButtomWithIcom = ({ text, icon, type }: ButtomWithIcomProps) => {
+  return (
+    <Root text={text} icon={icon} type={type}>
+      <div>{icon}</div>
+      {text && <span>{text}</span>}
+    </Root>
+  );
+};
+
+export default ButtomWithIcom;
